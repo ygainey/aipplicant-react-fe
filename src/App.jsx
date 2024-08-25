021e68ef7ff7c9a8ec1743fd04ed144094e13ec3
YGainey <yousef.gainey@gmail.com>
2024-08-25 18:36:50 +0100

    set up apps init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import { useState, createContext, useEffect } from 'react'
 import './App.css'
+import * as authService from './services/authService'
+
+export const AuthedUserConext = createContext(null)
 
 function App() {
+  const [user, setUser] = useState(authService.getUser())
 
-  return 
+  const handleSignOut = () =>{
+    authService.signOut()
+    setUser(null)
+  }
+
+  return
+
 }
 export default App
